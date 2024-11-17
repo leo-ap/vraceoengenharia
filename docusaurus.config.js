dd2cf91f2ae65fb1c301660416f80de9ebcded2f
le056548 <leonardoap@outlook.com>
2024-11-17 14:07:05 -0300

    Update site URL in docusaurus.config.js to reflect new domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   tagline: 'VRACEO',
   favicon: 'img/favicon.ico',
 
-  url: 'https://vraceoengenharia.github.io',
+  url: 'https://vraceoengenharia.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/vraceoengenharia/',
